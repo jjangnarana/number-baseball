115149b9544b6702fbf45618b11238c1b5e19a11
kimtokki <jjangnarana@gmail.com>
2023-11-13 09:04:23 +0900

    feat: Implement 'go' and 'refresh' button functionalities
    - Add onclick event handler for 'go' button to initiate process
    - Implement refresh functionality in 'refresh' button yo reload data
    - Improve diplay UI for better user experience

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
 import './App.css';
+import Logo from './logo.svg'
 import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <a href="http://localhost:3000/">
+          <img src={Logo} alt="" className='logo'/>
+        </a>
         <h1>숫자야구게임</h1>
+      </header>
         <NumberDisplay />
     </div>
   );
 }
 
 const NumberDisplay = () => {
-  const [selectedNumber, setSelectedNumber] = useState(null);
+  const [selectedNumbers, setSelectedNumbers] = useState(null);
 
   const handleClick = (number) => {
-    setSelectedNumber(number);
-  }
+    setSelectedNumbers(prevNumbers => {
+      if (prevNumbers === null) {
+        return [number];
+      }
+      if (!prevNumbers.includes(number) && prevNumbers.length < 4) {
+        return [...prevNumbers, number];
+      }
+      return prevNumbers;
+    });
+  };
 
+  const handleRefresh = () => {
+    setSelectedNumbers([]);
+  }
   return (
     <div className='btns'>
       <div className='display'>
-        {selectedNumber !== null ? selectedNumber : ''}
+        {selectedNumbers !== null ? selectedNumbers.map((number, index) => (
+          <div key={index}>{number}</div>
+        )) :''}
       </div>
       <div className='go-refresh'>
-        <button className='go'>go</button>
-        <button className='refresh'>refresh</button>
+        <button 
+          className='go'
+          disabled={selectedNumbers && selectedNumbers.length === 4 ? false : true}
+        >go!</button>
+        <button className='refresh' onClick={handleRefresh}>refresh</button>
       </div>
       <div className="number-grid">
         {Array.from({ length : 9}, (_, i) => i + 1).map(number => (
-          <button className="number-button" key={number} onClick={() => handleClick(number)}>
+          <button
+            className="number-button"
+            key={number}
+            onClick={() => handleClick(number)}
+            disabled={selectedNumbers && selectedNumbers.includes(number)}
+            >
             {number}
           </button>
         ))}
